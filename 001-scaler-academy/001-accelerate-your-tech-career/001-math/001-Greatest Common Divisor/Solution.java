2217a8f88b6ab402da793f90d3b56a58d826910c
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-03-27 23:47:05 +0530

    Update Solution.java

---
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/001-Greatest Common Divisor/Solution.java
@@ -0,0 +1,14 @@
+package io.github.raveerocks;
+
+public class Solution {
+    public int gcd(int A, int B) {
+        
+        if(B==0){
+            return A;
+        }
+        else
+        {
+            return gcd(B,A%B);
+        }
+    }
+}
