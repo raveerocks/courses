f2eb1e8fa59291c1fd62498678da627b937d411b
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-03-27 23:51:19 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/001-Greatest Common Divisor/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/001-Greatest Common Divisor/Solution.java
@@ -1,6 +1,13 @@
 package io.github.raveerocks;
 
 public class Solution {
+    
+     /**
+     *
+     * @param A : First Number
+     * @param B : Second Number
+     * @return  : GCD
+     */
     public int gcd(int A, int B) {
         
         if(B==0){
