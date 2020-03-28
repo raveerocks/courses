a2a5f5e4868c070f6aa2e9d1ce8e2c2b2f7ba5b1
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-03-28 19:02:12 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/001-Rain Water Trapped/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/001-Rain Water Trapped/Solution.java
@@ -11,28 +11,23 @@ public class Solution {
         int n = A.length;
         int maxLeftHeight[] = new int[n];
         int maxRightHeight[] = new int[n];
-        int currentLeftHeight, currentRightHeight;
-        int totalWaterTrapped = 0;
 
         // Scanning through the array to find the maxLeftHeight for each gap
-        currentLeftHeight = 0;
+        int currentLeftHeight = 0;
         for (int i = 0; i < n; i++) {
             maxLeftHeight[i] = currentLeftHeight;
-            if (currentLeftHeight < A[i]) {
-                currentLeftHeight = A[i];
-            }
+            currentLeftHeight = Math.max(currentLeftHeight,A[i]);
         }
 
         // Scanning through the array to find the maxRightHeight for each gap
-        currentRightHeight = 0;
+        int currentRightHeight = 0;
         for (int i = n - 1; i >= 0; i--) {
             maxRightHeight[i] = currentRightHeight;
-            if (currentRightHeight < A[i]) {
-                currentRightHeight = A[i];
-            }
+            currentRightHeight = Math.max(currentRightHeight,A[i]);
         }
 
         // Calculating the total water trapped within the gaps
+        int totalWaterTrapped=0;
         for (int i = 0; i < n; i++) {
             totalWaterTrapped += Math.max(0, Math.min(maxLeftHeight[i], maxRightHeight[i]) - A[i]);
         }
