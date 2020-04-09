c4647509022355e1dea05600725fecb3a804950c
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-04-09 11:05:27 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/002-Maximum Absolute Difference/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/002-Maximum Absolute Difference/Solution.java
@@ -8,28 +8,14 @@ public class Solution {
      */
     public int maxArr(int[] A) {
 
-        int AplusMin = Integer.MAX_VALUE, AplusMax = Integer.MIN_VALUE;
-        int AminusMin = Integer.MAX_VALUE, AminusMax = Integer.MIN_VALUE;
+        int AplusMin=A[0],AplusMax=A[0],AminusMin=A[0],AminusMax=A[0],max = 0;
 
-        int[] Aminus = new int[A.length];
-        int max = 0;
-
-        for (int i = 0; i < A.length; i++) {
-            if (i == 0) {
-                AplusMin = AplusMax = A[i] + i;
-                AminusMin = AminusMax = A[i] - i;
-
-            } else {
-
+        for (int i = 1; i < A.length; i++) {
                 AplusMin = Math.min(AplusMin, A[i] + i);
                 AplusMax = Math.max(AplusMax, A[i] + i);
                 AminusMin = Math.min(AminusMin, A[i] - i);
                 AminusMax = Math.max(AminusMax, A[i] - i);
-
-            }
-
         }
-
         return Math.max(AplusMax - AplusMin, AminusMax - AminusMin);
 
     }
