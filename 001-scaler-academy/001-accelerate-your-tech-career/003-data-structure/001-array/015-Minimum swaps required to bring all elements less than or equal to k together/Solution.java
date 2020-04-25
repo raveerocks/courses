15d99fb6f1b9b051bc5afa3a48e16362d4490c86
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-04-25 12:28:00 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/015-Minimum swaps required to bring all elements less than or equal to k together/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/015-Minimum swaps required to bring all elements less than or equal to k together/Solution.java
@@ -9,36 +9,25 @@ public class Solution {
         
         int count=0;
         
-        // Couting number of integers less than or equal to B
+         // Couting number of integers less than or equal to B
         for(int i =0; i<A.length;i++){
-            if(A[i]<=B){
-                count++;
-            }
+            count = A[i]<=B? count+1:count;
         }
         
         int exchanges=0;
         
         // In the first bracket of size 'count' checking how many exchanges are required
         for(int i=0;i<count;i++){
-            if(A[i]>B){
-                exchanges++;
-            }
+            exchanges = A[i]>B? exchanges+1 : exchanges;
         }
         
         int min = exchanges;
-        
-        // Among all brackets of size 'count' checking minimum exchanges required
+    
+       // Among all brackets of size 'count' checking minimum exchanges required
         for(int i=count; i<A.length;i++){
-            if(A[i-count]>B){
-                --exchanges;
-            }
-            if(A[i]>B){
-                ++exchanges;
-            }
-            
-            if(min>exchanges){
-                min = exchanges;
-            }
+            exchanges = A[i-count]>B?exchanges-1:exchanges;
+            exchanges = A[i]>B?exchanges+1:exchanges;
+            min = Math.min(min,exchanges);
         }
         return min;
     }
