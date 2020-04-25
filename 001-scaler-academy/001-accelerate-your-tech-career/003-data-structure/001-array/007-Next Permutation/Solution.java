46df76def71cb3a77dfc8cfb9af9402de9e8401a
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-04-25 11:11:05 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/007-Next Permutation/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/003-data-structure/001-array/007-Next Permutation/Solution.java
@@ -1,52 +1,49 @@
 package io.github.raveerocks;
 
-import java.util.ArrayList;
-
 public class Solution {
-    public ArrayList<Integer> nextPermutation(ArrayList<Integer> A) {
-
-        int n = A.size();
-        int last = A.get(n-1);
+    
+    public int[] nextPermutation(int[] A) {
+        
+        int n = A.length;
+        int last = A[n-1];
         int i = n-2;
-
+        
         // looping through increments
-
-        while(i>=0 && A.get(i)>=last ){
-            last = A.get(i);
+        
+        while(i>=0 && A[i]>=last ){
+            last = A[i];
             --i;
         }
-
+        
         if(i==-1){
             reverse(A,0,n-1);
             return A;
         }
-
-
-        int replacement = A.get(i);
-
+        
+  
+        int replacement = A[i];
+        
         int j=n-1;
-
-        while(A.get(j)<replacement ){
+        
+        while(A[j]<replacement ){
             j--;
         }
-
-        A.set(i, A.get(j));
-        A.set(j, replacement);
-
+        
+        A[i] = A[j];
+        A[j] = replacement;
+        
         reverse(A,i+1,n-1);
-
+        
         return A;
-
+        
     }
-
-
-    public void reverse(ArrayList<Integer> A, int from, int to){
-        int i;
-
+    
+    
+    public void reverse(int[] A, int from, int to){
         while(from<to){
-            i= A.get(from);
-            A.set(from,A.get(to));
-            A.set(to,i);
+            int temp= A[from];
+            A[from] = A[to];
+            A[to]=temp;
             from++;
             to--;
         }
