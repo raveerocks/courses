ed5d61b28a85774306ea9cf90c5cd57cda9239e6
raveendra.tudangil <raveendra@greayatom.com>
2020-05-01 22:54:17 +0530

    Range Sum

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/012-Range Sum/README.md
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/012-Range Sum/README.md
@@ -1,32 +1,55 @@
-#
+#Range Sum
 
 ## Problem Description
-
-
+Given two integers **A** and **B** such that **A** <= **B**. A Function **F** is defined as follows:
+```
+F[0] = 0
+F[1] = 1
+F[n] = F[n-1] + F[n-2]; n > 1
+```
+Function **S(A, B) = F[A] + F[A+1] + F[A+2] + ... + F[B]**. Find and return **S(A, B)** modulo **(10<sup>9</sup>+7)**.  
 ## Problem Constraints
 ```
-
+0 <= A <= B <= 10^9
 ```
 
 ## Input Format
 ```
-
+The arguments given are two integers A and B.
 ```
 
 ## Output Format
 ```
-
+Return an integer denoting the value of S(A, B) modulo (10^9+7).
 ```
-## Example
+## Example 1
 ```
-Input 1:
+Input :
+ A = 0
+ B = 3
 
-
-Output 1:
-
-```
-
-## Example Explanation
+Output :
+ 4
 ```
 
+## Example Explanation 1
+```
+ F(0) = 0, F(1) = 1, F(2) = 1, F(3) = 2.
+ S(0, 3) = F(0) + F(1) + F(2) + F(3) = 0 + 1 + 1 + 2 = 4.
+```
+
+## Example 2
+```
+Input :
+ A = 3
+ B = 4
+
+Output :
+ 5
+```
+
+## Example Explanation 1
+```
+  F(3) = 2, F(4) = 3.
+  S(3, 4) = F(3) + F(4) = 2 + 3 = 5.
 ```
---
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/012-Range Sum/Solution.java
@@ -0,0 +1,45 @@
+package io.github.raveerocks;
+
+public class Solution {
+    private int modulus = 1000000007;
+
+    public int solve(int A, int B) {
+
+        long c = (long)modulus* modulus;
+
+        int fibA = fibonacci(A+1);
+        int fibB = fibonacci(B+2);
+        return  (fibB-fibA+modulus)%modulus;
+
+    }
+
+
+    private int fibonacci(int n){
+        long [][] f = {{1,1},{1,0}};
+        long fib[][]  = matrixExponentiation(f,n-1);
+        return (int)fib[0][0];
+    }
+    private long[][] matrixExponentiation(long base[][], int exponent){
+        long [] [] result = {{1,0},{0,1}};
+        while (exponent>0){
+            if((exponent&1)==1){
+                result = matrixMultiplication(result,base);
+            }
+            base = matrixMultiplication(base,base);
+            exponent>>=1;
+        }
+        return result;
+    }
+
+    private long[][] matrixMultiplication(long A[][], long B[][]){
+        long c[][] = new long[2][2];
+
+        c[0][0] = ((A[0][0]*B[0][0])+(A[0][1]*B[1][0]))%modulus;
+        c[0][1] = ((A[0][0]*B[0][1])+(A[0][1]*B[1][1]))%modulus;
+        c[1][0] = ((A[1][0]*B[0][0])+(A[1][1]*B[1][0]))%modulus;
+        c[1][1] = ((A[1][0]*B[0][1])+(A[1][1]*B[1][1]))%modulus;
+        return c;
+    }
+
+
+}
