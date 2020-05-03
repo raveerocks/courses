7fa72f78c694d10599898177e5d89ecc10f36e23
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-05-03 22:49:54 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/020-Compute nCr % m/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/020-Compute nCr % m/Solution.java
@@ -2,12 +2,16 @@ package io.github.raveerocks;
 
 public class Solution {
     public int solve(int n, int r, int m) {
-         r = 2*r>n? n-r:r;
+        
+         // nCr = nCn-r, so we can use min(r,n-r) for optimization
+         r = Math.min(r,n-r);
 
+        // combinations[n+1][r+1] is used to store intermediate ncr values 
          int[] [] combinations = new int[n+1][r+1];
 
          for (int i=0; i<=n; i++){
              for (int j=0; j<=Math.min(i,r); j++){
+                 // nCr = (n-1nCr-1)+(n-1Cr)
                  combinations[i][j] = (i==0||j==0)?  1 : (combinations[i-1][j-1]+combinations[i-1][j])%m;
              }
          }
