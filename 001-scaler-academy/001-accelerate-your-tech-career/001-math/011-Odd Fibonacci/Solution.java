c64849faf5766e636fa130a070b1b686ca1b4c5e
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-05-03 14:04:05 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/011-Odd Fibonacci/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/011-Odd Fibonacci/Solution.java
@@ -2,22 +2,8 @@ package io.github.raveerocks;
 
 public class Solution {
     public int solve(int A, int B) {
-        
-        int position = A % 3;
-        int even=0;
-        
-        switch(position){
-            case 0:
-                even = 1+((B-A)/3);
-                break;
-            case 1:
-                even = 1+((B-A-2)/3);
-                break;
-            case 2 :
-                even = 1+((B-A-1)/3);
-                break;
-        }
-        return even>0?(B-A+1-even):B-A;
-        
+        int total = B-A+1;
+        int evenFibonacci = B/3 - (A-1)/3;
+        return total - evenFibonacci;
     }
 }
