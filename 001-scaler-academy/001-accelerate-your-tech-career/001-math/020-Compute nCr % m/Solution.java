961972a5e0c62cc7d5e74eafa7cf109b5087be1d
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-05-03 22:45:26 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/020-Compute nCr % m/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/020-Compute nCr % m/Solution.java
@@ -7,15 +7,10 @@ public class Solution {
          int[] [] combinations = new int[n+1][r+1];
 
          for (int i=0; i<=n; i++){
-             combinations[i][0] = 1;
-         }
-
-         for (int i=1; i<=n; i++){
-             for (int j=1; j<=Math.min(i,r); j++){
-                 combinations[i][j] = (combinations[i-1][j-1]+combinations[i-1][j])%m;
+             for (int j=0; j<=Math.min(i,r); j++){
+                 combinations[i][j] = (i==0||j==0)?  1 : (combinations[i-1][j-1]+combinations[i-1][j])%m;
              }
          }
-
         return combinations[n][r];
     }
 }
