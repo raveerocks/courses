a482386b1f326d80bb2e8dcbb7651d5ba89aba1f
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-05-03 13:51:42 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/010-Enumerating GCD/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/010-Enumerating GCD/Solution.java
@@ -2,13 +2,6 @@ package io.github.raveerocks;
 
 public class Solution {
     public String solve(String A, String B) {
-        
-        if(A.equals(B))
-        {
-            return A;
-        }
-        else{
-            return "1";
-        }
+        return A.equals(B)? A : "1";
     }
 }
