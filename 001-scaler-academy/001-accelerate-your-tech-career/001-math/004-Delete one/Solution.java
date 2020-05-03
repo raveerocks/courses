552e672e642f20155badfad7e38f0527246c700f
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-05-03 13:25:18 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/004-Delete one/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/004-Delete one/Solution.java
@@ -1,46 +1,42 @@
-package io.github.raveerocks;
-
-import java.util.ArrayList;
-
 public class Solution {
-
+    
     /**
      *
      * @param A : Array of integers
      * @return  : Maximum GCD Excluding a Single element
      */
-    public int solve(ArrayList<Integer> A) {
+    public int solve(int[] A) {
         
-        int n= A.size();
-        int prefixGCD [] =new int[n];
-        int suffixGCD [] =new int[n];
-        int excludedGCD[] = new int[n];
-        
-        prefixGCD[0] = A.get(0);
-        suffixGCD[n-1] = A.get(n-1);
-        excludedGCD[0] = suffixGCD[1];
-        excludedGCD[n-1] = prefixGCD[n-2];
-        
+        int n= A.length;
         
         
         // Calculating GCD from  left end to each element
+        int prefixGCD [] =new int[n];
+        prefixGCD[0] = A[0];
         for(int i=1;i<n;i++){
-            prefixGCD [i] = gcd(prefixGCD[i-1],A.get(i));
+            prefixGCD [i] = gcd(prefixGCD[i-1],A[i]);
         }
+        
         
         // Calculating GCD from  right end to each element
+        int suffixGCD [] =new int[n];
+        suffixGCD[n-1] = A[n-1];
         for(int i=n-2;i>=0;i--){
-            suffixGCD [i] = gcd(suffixGCD[i+1],A.get(i));
+            suffixGCD [i] = gcd(suffixGCD[i+1],A[i]);
         }
         
+        
         // Calculating excludedGCD from prefixGCD and suffixGCD
+        int excludedGCD[] = new int[n];
+        excludedGCD[0] = suffixGCD[1];
+        excludedGCD[n-1] = prefixGCD[n-2];
         for(int i=1; i< n-1; i++){
             excludedGCD[i] = gcd(prefixGCD [i-1],suffixGCD[i+1]);
         }
         
        
+       // Calculating maxGCD
        int maxGCD = excludedGCD[0];
- 
         for(int i =1; i<n; i++){
             maxGCD = maxGCD<excludedGCD[i]?excludedGCD[i]:maxGCD;
         }
@@ -48,21 +44,8 @@ public class Solution {
         return maxGCD;
         
     }
-
-
-
-    /**
-     *
-     * @param a : First Number
-     * @param b : Second Number
-     * @return  : GCD
-     */
-    private int gcd(int a, int b){
-        if(b==0){
-            return a;
-        }
-        else{
-            return gcd(b,a%b);
-        }
+    
+    public int gcd(int A, int B) {
+        return B==0? A:gcd(B,A%B);
     }
 }
