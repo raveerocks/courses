0dd3639ef9c52b9e93e47aea078b972d7e4ffb5e
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-05-03 16:20:43 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/015-Find nth Magic Number/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/015-Find nth Magic Number/Solution.java
@@ -1,11 +1,10 @@
+package io.github.raveerocks;
+
 public class Solution {
     public int solve(int A) {
-        int power = 5,result=0;
-        while(A>0){
-            int bit  = A&1;
-            A>>=1;
-            result += bit*power;
-            power*=5;
+        int power = 5, result=0;
+        for(int n=A; n>0; n>>=1, power*=5){
+            result +=(n&1)==1?power:0;
         }
         return result;
     }
