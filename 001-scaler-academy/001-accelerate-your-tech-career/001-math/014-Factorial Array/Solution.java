64386d4c7ad79dbe43037f9353062e129b714ceb
Raveendra Tudangil <53185091+raveerocks@users.noreply.github.com>
2020-05-03 14:52:18 +0530

    Update Solution.java

--- 001-scaler-academy/001-accelerate-your-tech-career/001-math/014-Factorial Array/Solution.java
+++ 001-scaler-academy/001-accelerate-your-tech-career/001-math/014-Factorial Array/Solution.java
@@ -1,54 +1,57 @@
+package io.github.raveerocks;
+
 import java.util.*;
 
 public class Solution {
-    public int solve(int[] A) {
+   public int solve(int[] A) {
 
-        int max = A[0];
-        final int result[]={0};
-
-        for (int i = 1; i < A.length; i++) {
-            max = Math.max(max, A[i]);
-        }
-
-        boolean isPrime[] = new boolean[max + 1];
-
-        for (int i = 0; i <= max; i++) {
-            isPrime[i] = true;
-        }
-
-        for (int i = 2; i*i <= max; i++) {
-            if(isPrime[i]){
-                for (int j = i*i; j <= max; j +=i) {
-                    isPrime[j]=false;
-                }
-            }
-        }
-
-        int nearestPrime[] = new int[max + 1];
-        int primes[] = new int[max + 1];
-        Map<Integer,Integer> count = new HashMap<>();
-        int lastPrime=1;
-
-        for(int i=1; i<=max; i++){
-            if(isPrime[i]){
-                lastPrime = i;
-                count.put(i,0);
-            }
-            nearestPrime[i] = lastPrime;
-        }
-
+        int max = max(A);
+        int nearestPrime[] =nearestPrime(max);
+        Map<Integer,Integer> countMap = new HashMap<>();
 
         for (int i=0;i<A.length;i++){
             int prime = nearestPrime[A[i]];
-            count.put(prime,count.get(prime)+1);
+            countMap.put(prime,countMap.getOrDefault(prime,0)+1);
         }
 
-        count.forEach((prime,currentCount) ->{
-            if(prime!=1){
-                result[0]+=Math.pow(2,currentCount)-1;
+        int result = 0;
+        for (int key : countMap.keySet()){
+            if (key!=1){
+                result += Math.pow(2,countMap.get(key))-1;
             }
-        });
-
-        return result[0];
+        }
+        return result;
+    }
+    private int max(int[] A){
+        int max = A[0];
+        for (int i = 1; i < A.length; i++) {
+            max = Math.max(max, A[i]);
+        }
+        return max;
+    }
+    private int[]  nearestPrime(int max){
+        boolean[] isPrime = isPrime(max+1);
+        int[] nearestPrime = new int[max+1];
+        
+        int lastPrime=1;
+        for(int i=1; i<=max; i++){
+            lastPrime = isPrime[i]?i:lastPrime;
+            nearestPrime[i] = lastPrime;
+        }
+        return nearestPrime;
+    }
+    private boolean[] isPrime(int max){
+        boolean result[] = new boolean[max + 1];
+        for (int i = 0; i <= max; i++) {
+            result[i] = true;
+        }
+        for (int i = 2; i*i <= max; i++) {
+            if(result[i]){
+                for (int j = i*i; j <= max; j +=i) {
+                    result[j]=false;
+                }
+            }
+        }
+        return result;
     }
 }
